e4bafc16ac68ba79160e99e7055fcd4b07d76fb5
oryants3 <oryants3@gmail.com>
2022-05-31 12:30:43 +0300

    Add project files.

---
+++ SQL project/Functions.cs
@@ -0,0 +1,259 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+namespace SQL_project
+{
+    internal class Functions
+    {
+       public static void Selectstore()
+       {
+            try
+            {
+                SqlConnection mySqlConnection = new SqlConnection("server=LocalHost\\SQLEXPRESS;database=project;Integrated Security=SSPI;");
+                SqlCommand mySqlCommand = mySqlConnection.CreateCommand();
+                mySqlConnection.Open();
+                mySqlCommand.CommandText = "select * from store";
+
+                SqlDataReader mySqlDataReader = mySqlCommand.ExecuteReader();
+                
+
+                for (int i = 0; i < mySqlDataReader.FieldCount; i++)
+                    Console.Write("  {0,-11}", mySqlDataReader.GetName(i));
+                Console.WriteLine();
+
+                for (int i = 0; i < mySqlDataReader.FieldCount; i++)
+                    Console.Write("~~~~~~~~~~");
+                Console.WriteLine("\n");
+
+                while (mySqlDataReader.Read())
+                {
+                    for (int i = 0; i < mySqlDataReader.FieldCount; i++)
+                        Console.Write("  {0,-11}", mySqlDataReader[i].ToString());
+                    Console.WriteLine();
+                }
+                mySqlDataReader.Close();
+                mySqlConnection.Close();
+            }
+            catch (Exception err)
+            {
+                Console.WriteLine(err.Message);
+            }
+       }
+        
+    
+        public static void InsertProduct()
+        {
+            try
+            {
+                
+                SqlConnection mySqlConnection = new SqlConnection("server=LocalHost\\SQLEXPRESS;database=project;Integrated Security=SSPI;");
+                SqlCommand mySqlCommand = mySqlConnection.CreateCommand();
+                mySqlConnection.Open();
+                        Console.Clear();
+                        Console.WriteLine("insert products to store: ");
+                        Console.Write("insert prodid: ");
+                        string a1 = Console.ReadLine();
+                        Console.Write("insert proname : ");
+                        string a2 = Console.ReadLine();
+                        Console.Write("insert price : ");
+                        int a3 = int.Parse(Console.ReadLine());
+                        
+                        mySqlCommand.CommandText = $"insert into store values('{a1}','{a2}','{a3}')";
+                int n = mySqlCommand.ExecuteNonQuery();
+                Console.WriteLine("Product inserted");
+                Selectstore();
+                mySqlConnection.Close();
+            }
+            catch (Exception err)
+            {
+                Console.WriteLine(err.Message);
+            }
+        }
+        public static void selectavg()
+        {
+            try
+            {
+                SqlConnection mySqlConnection = new SqlConnection("server=LocalHost\\SQLEXPRESS;database=project;Integrated Security=SSPI;");
+                SqlCommand mySqlCommand = mySqlConnection.CreateCommand();
+                mySqlConnection.Open();
+                    Console.Clear();
+                mySqlCommand.CommandText = "select avg(price)  as '\n AVG' from store";
+
+                SqlDataReader mySqlDataReader = mySqlCommand.ExecuteReader();
+
+
+                for (int i = 0; i < mySqlDataReader.FieldCount; i++)
+                    Console.Write("  {0,-11}", mySqlDataReader.GetName(i));
+                Console.WriteLine();
+
+                for (int i = 0; i < mySqlDataReader.FieldCount; i++)
+                Console.WriteLine("~~~~~~~~");
+
+                while (mySqlDataReader.Read())
+                {
+                    for (int i = 0; i < mySqlDataReader.FieldCount; i++)
+                        Console.Write("  {0,-11}", mySqlDataReader[i].ToString());
+                    Console.WriteLine();
+                }
+                mySqlDataReader.Close();
+                mySqlConnection.Close();
+            }
+            catch (Exception err)
+            {
+                Console.WriteLine(err.Message);
+            }
+        }
+        public static void AVGprice()
+        {
+            try
+            {
+                SqlConnection mySqlConnection = new SqlConnection("server=LocalHost\\SQLEXPRESS;database=project;Integrated Security=SSPI;");
+                SqlCommand mySqlCommand = mySqlConnection.CreateCommand();
+                mySqlConnection.Open();
+                mySqlCommand.CommandText = "select * from store";
+                selectavg();
+                mySqlConnection.Close();
+            }
+            catch (Exception err)
+            {
+                Console.WriteLine(err.Message);
+            }
+        }
+        public static void Selectusers()
+        {
+            try
+            {
+                SqlConnection mySqlConnection = new SqlConnection("server=LocalHost\\SQLEXPRESS;database=project;Integrated Security=SSPI;");
+                SqlCommand mySqlCommand = mySqlConnection.CreateCommand();
+                mySqlConnection.Open();
+                    Console.Clear();
+                mySqlCommand.CommandText = "select * from client";
+
+                SqlDataReader mySqlDataReader = mySqlCommand.ExecuteReader();
+
+
+                for (int i = 0; i < mySqlDataReader.FieldCount; i++)
+                    Console.Write("  {0,-11}", mySqlDataReader.GetName(i));
+                Console.WriteLine();
+
+                for (int i = 0; i < mySqlDataReader.FieldCount; i++)
+                    Console.Write("~~~~~~~~~~~~~~");
+                Console.WriteLine("\n");
+
+                while (mySqlDataReader.Read())
+                {
+                    for (int i = 0; i < mySqlDataReader.FieldCount; i++)
+                        Console.Write("  {0,-11}", mySqlDataReader[i].ToString());
+                }
+                mySqlDataReader.Close();
+                mySqlConnection.Close();
+            }
+            catch (Exception err)
+            {
+                Console.WriteLine(err.Message);
+            }
+        }
+
+        public static void Register(string id,string fname,string city)
+        {
+            try
+            {
+                SqlConnection mySqlConnection = new SqlConnection("server=LocalHost\\SQLEXPRESS;database=project;Integrated Security=SSPI;");
+                SqlCommand mySqlCommand = mySqlConnection.CreateCommand();
+                mySqlConnection.Open();
+                    Console.Clear();
+                mySqlCommand.CommandText = $"insert into client values('{id}','{fname}','{city}');";
+
+                int n = mySqlCommand.ExecuteNonQuery();
+                Console.WriteLine("Account Registerd ");
+                mySqlConnection.Close();
+               
+            }
+            catch (Exception err)
+            {
+                Console.WriteLine(err.Message);
+            }
+        }
+        
+        public static void Selectname()
+        {
+            try
+            {
+
+                SqlConnection mySqlConnection = new SqlConnection("server=LocalHost\\SQLEXPRESS;database=project;Integrated Security=SSPI;");
+                SqlCommand mySqlCommand = mySqlConnection.CreateCommand();
+                mySqlConnection.Open();
+                Console.Clear();
+                Console.WriteLine("\tWellcom To Users Manegment!\n");
+                mySqlCommand.CommandText = $"select * from client";
+                Console.WriteLine("\nShowing Users....\n");
+                SqlDataReader mySqlDataReader = mySqlCommand.ExecuteReader();
+
+
+                for (int i = 0; i < mySqlDataReader.FieldCount; i++)
+                    Console.Write("  {0,-11}", mySqlDataReader.GetName(i));
+                Console.WriteLine();
+
+                for (int i = 0; i < mySqlDataReader.FieldCount; i++)
+                    Console.Write("~~~~~~~~~~~~");
+                Console.WriteLine("\n");
+
+                while (mySqlDataReader.Read())
+                {
+                    for (int i = 0; i < mySqlDataReader.FieldCount; i++)
+                        Console.Write("  {0,-11}", mySqlDataReader[i].ToString());
+                    Console.WriteLine();
+                }
+                mySqlDataReader.Close();
+                mySqlConnection.Close();
+            }
+            catch (Exception err)
+            {
+                Console.WriteLine(err.Message);
+            }
+        }
+
+        public static void DeleteProd()
+        {
+            try
+            {
+                SqlConnection mySqlConnection = new SqlConnection("server=LocalHost\\SQLEXPRESS;database=project;Integrated Security=SSPI;");
+                SqlCommand mySqlCommand = mySqlConnection.CreateCommand();
+                mySqlConnection.Open();
+                Console.Clear();
+                int num;
+                Console.WriteLine("insert price to delete");
+                num = int.Parse(Console.ReadLine());
+                mySqlCommand.CommandText = $"delete from store where price = {num};";
+                SqlDataReader mySqlDataReader = mySqlCommand.ExecuteReader();
+
+                for (int i = 0; i < mySqlDataReader.FieldCount; i++)
+                    Console.Write("  {0,-11}", mySqlDataReader.GetName(i));
+                Console.WriteLine();
+
+                for (int i = 0; i < mySqlDataReader.FieldCount; i++)
+                    Console.Write("~~~~~~~~~~~");
+                Console.WriteLine("\n");
+
+                while (mySqlDataReader.Read())
+                {
+                    for (int i = 0; i < mySqlDataReader.FieldCount; i++)
+                        Console.Write("  {0,-11}", mySqlDataReader[i].ToString());
+                    Console.WriteLine();
+                }
+
+
+                Console.WriteLine("Product deleted");
+                mySqlDataReader.Close();
+                mySqlConnection.Close();
+            }
+            catch (Exception err)
+            {
+                Console.WriteLine(err.Message);
+            }
+        }
+    }
+}
---
+++ SQL project/Program.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+
+namespace SQL_project
+{
+    internal class Program
+    {
+       
+
+    
+        static void Main(string[] args)
+        {
+            int num = 0;
+            while (num != 6)
+            {
+                
+                Console.ForegroundColor = ConsoleColor.Yellow;
+
+                Console.WriteLine("Choose your option: ");
+                Console.WriteLine("1 ~~~> insert product");
+                Console.WriteLine("2 ~~~> show product");
+                Console.WriteLine("3 ~~~> Show Users");
+                Console.WriteLine("4 ~~~> delete product");
+                Console.WriteLine("5 ~~~> Register");
+                Console.WriteLine("6.Exit");
+                num = int.Parse(Console.ReadLine());
+                switch (num)
+                {
+                    case 1:
+                        Console.ForegroundColor = ConsoleColor.DarkGreen;
+                        Functions.InsertProduct();
+                        Console.WriteLine("\n");
+
+                        break;
+
+                    case 2:
+                        Console.ForegroundColor = ConsoleColor.Blue;
+                        Functions.Selectstore();
+                        Console.WriteLine("\n");
+                        break;
+
+                    case 3:
+                        Console.ForegroundColor = ConsoleColor.DarkCyan;
+                        Functions.Selectname();
+                        Console.WriteLine("\n");
+
+                        break;
+
+                    case 4:
+                        Console.ForegroundColor = ConsoleColor.DarkGray;
+                        Functions.DeleteProd();
+                        Console.WriteLine("\n");
+
+                        break;
+
+                    case 5:
+                        Console.ForegroundColor = ConsoleColor.Blue;
+                        Console.WriteLine("Enter Your ID: ");
+                        string id = Console.ReadLine();
+                        Console.WriteLine("Enter Your Name: ");
+                        string fname = Console.ReadLine();
+                        Console.WriteLine("Enter Your Home City: ");
+                        string city = Console.ReadLine();
+                        Functions.Register(id, fname, city);
+                        Console.WriteLine("\n");
+
+                        break;
+
+                    case 6:
+                        Console.ForegroundColor = ConsoleColor.DarkGreen;
+                        
+                        Console.WriteLine("Thank you And Goodbye!");
+                        break;
+
+                }
+                if(num < 1 || num > 6 )
+                {
+                    Console.ForegroundColor = ConsoleColor.Red;
+                    Console.WriteLine("Not in List! Try Again Please....");
+                }
+
+            }
+            Console.WriteLine();
+
+        }
+    }
+}
